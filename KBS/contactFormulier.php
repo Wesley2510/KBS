aa33f69e2f7c840eafd0f7923169f55cbcbeb757
wesley oosterveen <wesleyoosterveen94@gmail.com>
2014-11-14 12:00:10 +0100

    Merge KBS/master
    Conflicts:
    	KBS/index.php

---
+++ KBS/contactFormulier.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+Wesley Oosterveen
+Studentnummer: s1084668
+klas: ICTM1K
+collegejaar: 2014-2015
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>contact Formulier</title>
+    </head>
+    <body>
+        <aside><a href="index.php"><h5>Terug naar hoofdpagina</h5></a></aside>
+
+        <h1>Hier komt het contact fomulier!</h1>
+
+
+        <?php
+        ?>
+    </body>
+</html>
