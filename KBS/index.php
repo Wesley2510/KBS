f263d7ee81b252f6117a28890ce0f5abcb878235
wesley oosterveen <wesleyoosterveen94@gmail.com>
2014-11-14 14:24:09 +0100

    Css

--- KBS/index.php
+++ KBS/index.php
@@ -11,7 +11,7 @@
 
         <ul>
             <li><a href="contactformulier.php"><h4 class="linkText">contact formulier</h4></a></li>
-            <li><a href="achtergrondInformatie.php"><h4>Achtergrond informatie</h4></a></li>
+            <li><a href="achtergrondInformatie.php"><h4>Achtergrond info</h4></a></li>
             <li><a href="klantenLogin.php"><h4>Klanten login</h4></a></li>
             <li><a href="beheerLogin.php"><h4>Beheer login</h4></a></li>
         </ul>
