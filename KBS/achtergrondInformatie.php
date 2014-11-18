9751d003d43a89cc66f9c6acedb53c0c968daf65
wesley oosterveen <wesleyoosterveen94@gmail.com>
2014-11-18 09:31:33 +0100

    Ricks code gecommit plus enkele kleine fixjes in index

--- KBS/achtergrondInformatie.php
+++ KBS/achtergrondInformatie.php
@@ -8,8 +8,13 @@ Wesley Oosterveen
         <meta charset="UTF-8">
         <title>Achtergrond infromatie</title>
     </head>
-    <body>
-        <aside><a href="index.php"><h5>Terug naar hoofdpagina</h5></a></aside>
+    <body><link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+        <ul>
+            <li><aside><a href="index.php"><h5>Terug naar hoofdpagina</h5></a></aside></li>
+            <li><aside><a href="contactFormulier.php"><h5>Contactformulier</h5></a></aside></li>
+            <li><aside><a href="klantenLogin.php"><h5>Klantlogin</h5></a></aside></li>
+        </ul>
         <h1>Hier komt de achtergrond informatie!</h1>
         <?php
         ?>
