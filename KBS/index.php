f2c83b14420a17d5538a08217d29a9bd86c0cc3a
wesley oosterveen <wesleyoosterveen94@gmail.com>
2014-11-18 11:50:17 +0100

    Index spelling aangepast, css omgegooit naar schermontwerp.

--- KBS/index.php
+++ KBS/index.php
@@ -10,12 +10,11 @@
         <link rel="stylesheet" type="text/css" href="stylesheet.css">
 
         <ul>
-            <li><h1 class="titel"> TextBug</h1></li>
-            <li><a href="contactformulier.php"><h4 class="linkText">contact formulier</h4></a></li>
+            <li><h5>TextBug</h5></li>
+            <li><a href="ontactformulier.php"><h4>Contact formulier</h4></a></li>
             <li><a href="achtergrondInformatie.php"><h4>Achtergrond info</h4></a></li>
-            <li><a href="klantenLogin.php"><h4>Klanten login</h4></a></li>
-            <li><a href="beheerLogin.php"><h4>Beheer login</h4></a></li>
             <li><a href="Contactgegevens.php"><h4>Contactgegevens</h4></a></li>
+            <li><a href="Login.php"><h4>Login</h4></a></li>
         </ul>
         <?php
         ?>
