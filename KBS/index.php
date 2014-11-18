2b1042b16c3c468d0602a0fd9280b636b307d19e
wesley oosterveen <wesleyoosterveen94@gmail.com>
2014-11-18 09:33:41 +0100

    (fix) Class verkeerd geschreven.

--- KBS/index.php
+++ KBS/index.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" type="text/css" href="stylesheet.css">
 
         <ul>
-            <li><h1 class="titels"> TextBug</h1></li>
+            <li><h1 class="titel"> TextBug</h1></li>
             <li><a href="contactformulier.php"><h4 class="linkText">contact formulier</h4></a></li>
             <li><a href="achtergrondInformatie.php"><h4>Achtergrond info</h4></a></li>
             <li><a href="klantenLogin.php"><h4>Klanten login</h4></a></li>
