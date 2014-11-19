ba02b10566128f5fd7c8aedf653f20c2d7b0ef16
lewis <lewis@lewis-MS-7850>
2014-11-19 18:42:22 +0100

    Simpele page-elements met responsive design toegevoegd

--- KBS/index.php
+++ KBS/index.php
@@ -9,13 +9,80 @@
 
         <link rel="stylesheet" type="text/css" href="stylesheet.css">
 
-        <ul>
-            <li><h5>TextBug</h5></li>
-            <li><a href="ontactformulier.php"><h4>Contact formulier</h4></a></li>
-            <li><a href="achtergrondInformatie.php"><h4>Achtergrond info</h4></a></li>
-            <li><a href="Contactgegevens.php"><h4>Contactgegevens</h4></a></li>
-            <li><a href="Login.php"><h4>Login</h4></a></li>
-        </ul>
+        <div>
+            <ul>
+                <li><h5>TextBug</h5></li>
+                <li><a href="ontactformulier.php"><h4>Contact formulier</h4></a></li>
+                <li><a href="achtergrondInformatie.php"><h4>Achtergrond info</h4></a></li>
+                <li><a href="Contactgegevens.php"><h4>Contactgegevens</h4></a></li>
+                <li><a href="Login.php"><h4>Login</h4></a></li>
+            </ul>
+        </div>
+        
+        <div class="pageElement">
+            <h2>Lorem ipsum dolor sit amet,</h2>
+            <p>consectetur adipiscing elit.
+                Curabitur id risus at augue eleifend vehicula.
+                Pellentesque vehicula rutrum mi ut vestibulum. 
+                Aliquam erat volutpat. Donec sed risus sapien. 
+                Curabitur iaculis fringilla blandit. 
+                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac. 
+                In vehicula lacus eleifend ornare tempor.
+                Mauris ultricies, eros sed tincidunt sodales, 
+                lectus ante maximus est, accumsan placerat mi velit non ante. 
+                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus. 
+                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis, 
+                dapibus orci nec, efficitur libero. Suspendisse molestie lectus id efficitur efficitur.
+                Pellentesque pretium porttitor lectus, non condimentum ipsum vehicula in. Donec vitae consequat elit. </p>
+        </div>
+        <div class="pageElement">
+            <h2>Lorem ipsum dolor sit amet,</h2>
+            <p>consectetur adipiscing elit.
+                Curabitur id risus at augue eleifend vehicula.
+                Pellentesque vehicula rutrum mi ut vestibulum. 
+                Aliquam erat volutpat. Donec sed risus sapien. 
+                Curabitur iaculis fringilla blandit. 
+                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac. 
+                In vehicula lacus eleifend ornare tempor.
+                Mauris ultricies, eros sed tincidunt sodales, 
+                lectus ante maximus est, accumsan placerat mi velit non ante. 
+                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus. 
+                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis, 
+                dapibus orci nec, efficitur libero. Suspendisse molestie lectus id efficitur efficitur.
+                Pellentesque pretium porttitor lectus, non condimentum ipsum vehicula in. Donec vitae consequat elit. </p>
+        </div>
+        <div class="pageElement">
+            <h2>Lorem ipsum dolor sit amet,</h2>
+            <p>consectetur adipiscing elit.
+                Curabitur id risus at augue eleifend vehicula.
+                Pellentesque vehicula rutrum mi ut vestibulum. 
+                Aliquam erat volutpat. Donec sed risus sapien. 
+                Curabitur iaculis fringilla blandit. 
+                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac. 
+                In vehicula lacus eleifend ornare tempor.
+                Mauris ultricies, eros sed tincidunt sodales, 
+                lectus ante maximus est, accumsan placerat mi velit non ante. 
+                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus. 
+                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis, 
+                dapibus orci nec, efficitur libero. Suspendisse molestie lectus id efficitur efficitur.
+                Pellentesque pretium porttitor lectus, non condimentum ipsum vehicula in. Donec vitae consequat elit. </p>
+        </div>
+        <div class="pageElement">
+            <h2>Lorem ipsum dolor sit amet,</h2>
+            <p>consectetur adipiscing elit.
+                Curabitur id risus at augue eleifend vehicula.
+                Pellentesque vehicula rutrum mi ut vestibulum. 
+                Aliquam erat volutpat. Donec sed risus sapien. 
+                Curabitur iaculis fringilla blandit. 
+                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac. 
+                In vehicula lacus eleifend ornare tempor.
+                Mauris ultricies, eros sed tincidunt sodales, 
+                lectus ante maximus est, accumsan placerat mi velit non ante. 
+                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus. 
+                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis, 
+                dapibus orci nec, efficitur libero. Suspendisse molestie lectus id efficitur efficitur.
+                Pellentesque pretium porttitor lectus, non condimentum ipsum vehicula in. Donec vitae consequat elit. </p>
+        </div>
         <?php
         ?>
     </body>
