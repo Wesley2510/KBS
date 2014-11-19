df089fd348e80ae5bad26171259fd62e117a8db7
wesley oosterveen <wesleyoosterveen94@gmail.com>
2014-11-19 17:08:41 +0100

    css aangepast

--- KBS/index.php
+++ KBS/index.php
@@ -10,8 +10,8 @@
         <link rel="stylesheet" type="text/css" href="stylesheet.css">
 
         <ul class='lastAlignRight'>
-            <li><h5>TextBug</h5></li>
-            <li><a href="ontactformulier.php"><h4>Contact formulier</h4></a></li>
+            <li><h5 id="title">TextBug</h5></li>
+            <li><a href="contactFormulier.php"><h4>Contact formulier</h4></a></li>
             <li><a href="achtergrondInformatie.php"><h4>Achtergrond info</h4></a></li>
             <li><a href="Contactgegevens.php"><h4>Contactgegevens</h4></a></li>
             <li><a href="Login.php"><h4>Login</h4></a></li>
