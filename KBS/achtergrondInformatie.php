a5888d51677c0b80ac41164d3b6199361bb235e1
lewis <lewis@lewis-HP-G62-Notebook-PC>
2014-11-20 11:20:30 +0100

    Achtergrondinfo gebruik correcte tags en ID's

--- KBS/achtergrondInformatie.php
+++ KBS/achtergrondInformatie.php
@@ -7,14 +7,15 @@ Wesley Oosterveen
     <head>
         <meta charset="UTF-8">
         <title>Achtergrond infromatie</title>
-    </head>
-    <body><link rel="stylesheet" type="text/css" href="stylesheet.css">
 
-        <ul>
-            <li><aside><a href="index.php"><h5>Terug naar hoofdpagina</h5></a></aside></li>
-            <li><aside><a href="contactFormulier.php"><h5>Contactformulier</h5></a></aside></li>
-            <li><aside><a href="klantenLogin.php"><h5>Klantlogin</h5></a></aside></li>
-        </ul>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+    <body>
+        <nav id="headerbar">
+            <section><aside><a href="index.php"><h5>Terug naar hoofdpagina</h5></a></aside></section>
+            <section><aside><a href="contactFormulier.php"><h5>Contactformulier</h5></a></aside></section>
+            <section><aside><a href="klantenLogin.php"><h5>Klantlogin</h5></a></aside></section>
+        </nav>
         <h1>Hier komt de achtergrond informatie!</h1>
         <?php
         ?>
