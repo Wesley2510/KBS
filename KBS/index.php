2b1a05ddf4a5ea5bbfac63d1e2c142a10f27a62e
sander <sander@145.44.52.136>
2014-11-20 10:25:18 +0100

    contactgegevens.php ipv ontactgegevens.php

--- KBS/index.php
+++ KBS/index.php
@@ -11,25 +11,25 @@
 
         <nav id="headerbar">
             <section><h5>TextBug</h5></section>
-            <section><a href="ontactformulier.php"><h4>Contactformulier</h4></a></section>
+            <section><a href="contactformulier.php"><h4>Contactformulier</h4></a></section>
             <section><a href="achtergrondInformatie.php"><h4>Achtergrondinfo</h4></a></section>
             <section><a href="Contactgegevens.php"><h4>Contactgegevens</h4></a></section>
             <section><a href="Login.php"><h4>Login</h4></a></section>
         </nav>
-        
+
         <div class="pageElement">
             <h2>Lorem ipsum dolor sit amet,</h2>
             <p>consectetur adipiscing elit.
                 Curabitur id risus at augue eleifend vehicula.
-                Pellentesque vehicula rutrum mi ut vestibulum. 
-                Aliquam erat volutpat. Donec sed risus sapien. 
-                Curabitur iaculis fringilla blandit. 
-                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac. 
+                Pellentesque vehicula rutrum mi ut vestibulum.
+                Aliquam erat volutpat. Donec sed risus sapien.
+                Curabitur iaculis fringilla blandit.
+                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac.
                 In vehicula lacus eleifend ornare tempor.
-                Mauris ultricies, eros sed tincidunt sodales, 
-                lectus ante maximus est, accumsan placerat mi velit non ante. 
-                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus. 
-                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis, 
+                Mauris ultricies, eros sed tincidunt sodales,
+                lectus ante maximus est, accumsan placerat mi velit non ante.
+                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus.
+                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis,
                 dapibus orci nec, efficitur libero. Suspendisse molestie lectus id efficitur efficitur.
                 Pellentesque pretium porttitor lectus, non condimentum ipsum vehicula in. Donec vitae consequat elit. </p>
         </div>
@@ -37,15 +37,15 @@
             <h2>Lorem ipsum dolor sit amet,</h2>
             <p>consectetur adipiscing elit.
                 Curabitur id risus at augue eleifend vehicula.
-                Pellentesque vehicula rutrum mi ut vestibulum. 
-                Aliquam erat volutpat. Donec sed risus sapien. 
-                Curabitur iaculis fringilla blandit. 
-                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac. 
+                Pellentesque vehicula rutrum mi ut vestibulum.
+                Aliquam erat volutpat. Donec sed risus sapien.
+                Curabitur iaculis fringilla blandit.
+                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac.
                 In vehicula lacus eleifend ornare tempor.
-                Mauris ultricies, eros sed tincidunt sodales, 
-                lectus ante maximus est, accumsan placerat mi velit non ante. 
-                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus. 
-                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis, 
+                Mauris ultricies, eros sed tincidunt sodales,
+                lectus ante maximus est, accumsan placerat mi velit non ante.
+                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus.
+                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis,
                 dapibus orci nec, efficitur libero. Suspendisse molestie lectus id efficitur efficitur.
                 Pellentesque pretium porttitor lectus, non condimentum ipsum vehicula in. Donec vitae consequat elit. </p>
         </div>
@@ -53,15 +53,15 @@
             <h2>Lorem ipsum dolor sit amet,</h2>
             <p>consectetur adipiscing elit.
                 Curabitur id risus at augue eleifend vehicula.
-                Pellentesque vehicula rutrum mi ut vestibulum. 
-                Aliquam erat volutpat. Donec sed risus sapien. 
-                Curabitur iaculis fringilla blandit. 
-                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac. 
+                Pellentesque vehicula rutrum mi ut vestibulum.
+                Aliquam erat volutpat. Donec sed risus sapien.
+                Curabitur iaculis fringilla blandit.
+                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac.
                 In vehicula lacus eleifend ornare tempor.
-                Mauris ultricies, eros sed tincidunt sodales, 
-                lectus ante maximus est, accumsan placerat mi velit non ante. 
-                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus. 
-                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis, 
+                Mauris ultricies, eros sed tincidunt sodales,
+                lectus ante maximus est, accumsan placerat mi velit non ante.
+                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus.
+                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis,
                 dapibus orci nec, efficitur libero. Suspendisse molestie lectus id efficitur efficitur.
                 Pellentesque pretium porttitor lectus, non condimentum ipsum vehicula in. Donec vitae consequat elit. </p>
         </div>
@@ -69,15 +69,15 @@
             <h2>Lorem ipsum dolor sit amet,</h2>
             <p>consectetur adipiscing elit.
                 Curabitur id risus at augue eleifend vehicula.
-                Pellentesque vehicula rutrum mi ut vestibulum. 
-                Aliquam erat volutpat. Donec sed risus sapien. 
-                Curabitur iaculis fringilla blandit. 
-                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac. 
+                Pellentesque vehicula rutrum mi ut vestibulum.
+                Aliquam erat volutpat. Donec sed risus sapien.
+                Curabitur iaculis fringilla blandit.
+                Aliquam dignissim egestas nibh, et suscipit lacus ultricies ac.
                 In vehicula lacus eleifend ornare tempor.
-                Mauris ultricies, eros sed tincidunt sodales, 
-                lectus ante maximus est, accumsan placerat mi velit non ante. 
-                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus. 
-                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis, 
+                Mauris ultricies, eros sed tincidunt sodales,
+                lectus ante maximus est, accumsan placerat mi velit non ante.
+                Quisque porta at tellus ut interdum. Ut a nibh a tellus facilisis aliquam quis ut metus.
+                Mauris nec justo vulputate, sodales enim nec, pellentesque felis. Curabitur non purus mollis,
                 dapibus orci nec, efficitur libero. Suspendisse molestie lectus id efficitur efficitur.
                 Pellentesque pretium porttitor lectus, non condimentum ipsum vehicula in. Donec vitae consequat elit. </p>
         </div>
