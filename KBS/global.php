0c3970a206d1f55c9fa55912afc8789d59bd07a5
lewis <lewis@lewis-MS-7850>
2014-11-23 20:55:57 +0100

    global.php opgeschoond

--- KBS/global.php
+++ KBS/global.php
@@ -1,3 +1,7 @@
+/*
+Lewis Clement
+*/
+
 <?php
 
  $GLOBALS["link"] = new mysqli("127.0.0.1", "root", "password", "Textbug", 3306);
@@ -7,36 +11,27 @@ if ( $GLOBALS["link"]->connect_error) {
 }
 
 function printHeader() {
-   print("<nav id=\"headerbar\">
-            <section><h5>TextBug</h5></section>");
+   print("<nav id=\"headerbar\"><section><h5>TextBug</h5></section>");
             
-            //Haal alle text columns uit tabel Menuitem
-            $menuitems = $GLOBALS["link"]->query("SELECT naam, positie FROM pagina;");
-            if($menuitems === false) {
-                trigger_error("SQL: \"" . sql .  "\" \n\r Error: \"" . $GLOBALS["link"]->error, E_USER_ERROR);
-            } else {
-                $list = array();
-                
-                $menuitems->data_seek(0);
-                while($row = $menuitems->fetch_assoc()) {
-                    //Plaats de menuitems op de juiste positie in de array $list
-                    $list[$row["positie"]] = $row["naam"];
-                }
-                $list[0] = null;
-                
-                for($i = 1; $i <= count($list); $i++) {
-                    //Plaats de text columns met behulp van <section> tags in het menu
-                    echo "<section><a href =\"index.php?p=" . $list[$i] . "\" ><h4>" . $list[$i] . "</h4></a></section>";
-                }
-            }
-       
-     print("<section><a href=\"Login.php\"><h4>Login</h4></a></section>
-        </nav>");
+    //Haal alle text columns uit tabel Menuitem
+    $menuitems = $GLOBALS["link"]->query("SELECT naam, positie FROM pagina;");
+    if($menuitems === false) {
+        trigger_error("SQL: \"" . sql .  "\" \n\r Error: \"" . $GLOBALS["link"]->error, E_USER_ERROR);
+    } else {
+        $list = array();
+
+        $menuitems->data_seek(0);
+        while($row = $menuitems->fetch_assoc()) {
+            //Plaats de menuitems op de juiste positie in de array $list
+            $list[$row["positie"]] = $row["naam"];
+        }
+        $list[0] = NULL;
+
+        for($i = 1; $i <= count($list); $i++) {
+            //Plaats de text columns met behulp van <section> tags in het menu
+            echo "<section><a href =\"index.php?p=" . $list[$i] . "\" ><h4>" . $list[$i] . "</h4></a></section>";
+        }
+    }
+
+    print("<section><a href=\"Login.php\"><h4>Login</h4></a></section></nav>");
 }
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
