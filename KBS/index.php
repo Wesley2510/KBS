1701bc61de075b605ade59801ac10fa54bb26534
LewisClement <lewisclement@gmail.com>
2014-11-26 00:17:10 +0100

    De "Oudere berichten" knop wordt niet meer getoont als de laatste berichten precies op de huidige pagina passen.

--- KBS/index.php
+++ KBS/index.php
@@ -111,7 +111,7 @@ if($inputBerichtVerwijderID != NULL && is_numeric($inputBerichtVerwijderID)) {
             if($aantalBerichten["aantal"] > 5) {
                 echo "<div class='pageElement flexRowSpace'>";
                 
-                if( !( ($inputB+1)*5 > $aantalBerichten["aantal"]) ) {
+                if( !( ($inputB+1)*5 >= $aantalBerichten["aantal"]) ) {
                     echo "<a class='button' href='?p=" . $inputP . "&amp;b=" . ($inputB+1) . "'>Oudere berichten</a>";
                 } else { echo "<a></a>";} //Zodat de "Nieuwere berichten" knop rechts komt te staan
                 
