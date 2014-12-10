21a9e0bd969314c39bd58dcc6bfe0943d3caee15
LewisClement <lewisclement@gmail.com>
2014-12-10 21:35:14 +0100

    Meer error checks voor wachtwoord.

--- KBS/login.php
+++ KBS/login.php
@@ -6,11 +6,23 @@ include_once("global.php");
 $inputUsername = filter_input(INPUT_POST, "username");
 $inputPassword = filter_input(INPUT_POST, "password");
 $errorUsername = 0; //0 = No error, 1 = empty username
+$errorPassword = 0; //0 = No error, 1 = empty password
 if($inputUsername !== NULL) {
+    $succes = true;
     if(ltrim($inputUsername, ' ') === '') {
         $errorUsername = 1;
-    } else {
+        $succes = false;
+    }
+    
+    if(ltrim($inputPassword, ' ') === '') {
+        $errorPassword = 1;
+        $succes = false;
+    }
+    
+    if($succes) {
         header('Location: #');
+    } else {
+        $filledUsername = $inputUsername;
     }
 }
 
@@ -30,14 +42,19 @@ if($inputUsername !== NULL) {
         <form class="pageElement" action='#' id='loginForm' method="post">
             <div style="display:flex;flex-direction:column;align-items:center;">
                 <h3>Gebruikersnaam</h3>
-                <input type="text" name="username" <?php if($errorUsername > 0) {echo "class='error'";} ?> />
+                <input type="text" name="username" <?php if($errorUsername > 0) {echo "class='error'";} echo "value='" . $filledUsername . "'"; ?> />
                 <?php 
                     if($errorUsername == 1) {
                         echo "<h4 class='error'>Vul A.U.B. een gebruikersnaam in</h4>"; 
                     }
                 ?>
                 <h3>Wachtwoord</h3>
-                <input type="password" name="password"/>
+                <input type="password" name="password" <?php if($errorPassword > 0) {echo "class='error'";} ?>/>
+                <?php 
+                    if($errorPassword == 1) {
+                        echo "<h4 class='error'>Vul A.U.B. uw wachtwoord in</h4>"; 
+                    }
+                ?>
                 <a role="button" onclick="login();" style="margin: 0.5rem;">Login</a>
             </div>
         </form>
