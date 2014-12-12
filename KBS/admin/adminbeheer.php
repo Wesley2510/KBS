1d29be0936b38807f84ac8648fb07d6954d12a16
LewisClement <lewisclement@gmail.com>
2014-12-12 12:44:29 +0100

    Plaatjes op de site resizen aan de hand van de pageinabreedte.

---
+++ KBS/admin/adminbeheer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<?php
+include_once("../global.php");
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Textbug - Template</title>
+        <?php printStyles(); printScripts(); ?>
+    </head>
+    <body>
+    <?php printHeader(); ?>
+    
+    
+    
+    <?php printFooter(); ?>
+    </body>
+</html>
